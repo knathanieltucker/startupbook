efd049edeb65a90086fb8e6f4c67dc49015a6101
nate <you@example.com>
2018-03-11 22:44:35 -0700

    meteor-kitchen 201803120544351604

---
+++ startupbook/client/views/guide/preparation/preparation_controller.js
@@ -0,0 +1,50 @@
+this.GuidePreparationController = RouteController.extend({
+	template: "Guide",
+	
+
+	yieldTemplates: {
+		'GuidePreparation': { to: 'GuideSubcontent'}
+		
+	},
+
+	onBeforeAction: function() {
+		this.next();
+	},
+
+	action: function() {
+		if(this.isReady()) { this.render(); } else { this.render("Guide"); this.render("loading", { to: "GuideSubcontent" });}
+		/*ACTION_FUNCTION*/
+	},
+
+	isReady: function() {
+		
+
+		var subs = [
+			Meteor.subscribe("quote_list")
+		];
+		var ready = true;
+		_.each(subs, function(sub) {
+			if(!sub.ready())
+				ready = false;
+		});
+		return ready;
+	},
+
+	data: function() {
+		
+
+		var data = {
+			params: this.params || {},
+			quote_list: Quotes.find({}, {})
+		};
+		
+
+		
+
+		return data;
+	},
+
+	onAfterAction: function() {
+		
+	}
+});
